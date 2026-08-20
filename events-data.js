d86e8b27b78eb2d4f4ae292aad4c3bb55ddb97be
Kuldeep Yaduvanshi <kuldeepyaduvanshi281@gmail.com>
2026-08-20 21:59:21 +0530

    Add files via upload

---
+++ events-data.js
@@ -0,0 +1,37 @@
+const eventsData = [
+    {
+        id: 1,
+        title: 'Fund My Crazy — Build Night with Gemini',
+        org: 'AmbitionForge',
+        host: 'Kuldeep Yaduvanshi',
+        category: 'Product Trial',
+        description: 'Reimagine a real problem in your city, build your idea with Google Gemini and Nano Banana, and pitch your vision in a hands-on Build Night.',
+        date: '2026-08-23',
+        time: '8:00 PM',
+        location: 'Online',
+        link: 'https://producttrialsurvey.geministudentambassador.com/',
+        status: 'upcoming',
+        poster: 'assets/events/fundmycrazy.png',
+        gid: '10311',
+        ambassadorEmail: 'kuldeepyaduvanshi281@gmail.com'  // ← Email yahan
+    },
+
+    {
+    id: 1,
+    title: "Freshers' Party Planning Night",
+    org: 'AmbitionForge',
+    host: 'Kuldeep Yaduvanshi',
+    category: 'Product Trial',
+    description: "A hands-on Gemini Product Trial where students planned a freshers' party, explored Google's AI tools, earned a participation certificate, and competed for the winner certificate and a chance to be nominated for a Google Pixel phone.",
+    date: '2026-07-22',
+    time: '8:00 PM',
+    location: 'Online',
+    link: 'https://producttrialsurvey.geministudentambassador.com/',
+    status: 'past',
+    poster: 'assets/events/freshersparty.png',
+    gid: '10311',
+    ambassadorEmail: 'kuldeepyaduvanshi281@gmail.com'
+}
+
+
+];
